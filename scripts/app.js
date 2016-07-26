22867bfb61138e3cf034160bd07d39cc285bb1ee
Kamipown <paul.delobbel@gmail.com>
2016-07-26 02:47:21 +0200

    structure

--- scripts/app.js
+++ scripts/app.js
@@ -4,8 +4,10 @@ app.config(function($routeProvider)
 {
 	$routeProvider.when("/", {templateUrl: "partials/home.html"});
 	$routeProvider.when("/serveur", {templateUrl: "partials/server.html"});
+	$routeProvider.when("/regles", {templateUrl: "partials/rules.html"});
+	$routeProvider.when("/histoire", {templateUrl: "partials/story.html"});
+	$routeProvider.when("/personnages", {templateUrl: "partials/characters.html"});
 	$routeProvider.when("/carte", {templateUrl: "partials/map.html", controller: "map_ctrl"});
-	$routeProvider.when("/clans", {templateUrl: "partials/clans.html"});
 	$routeProvider.when("/mods", {templateUrl: "partials/mods.html"});
 	// $routeProvider.otherwise({templateUrl: "partials/notfound.html", controller: "notfound_ctrl"});
 });
